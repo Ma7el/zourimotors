9d940d130595c9e245eeb266c123e6febc218980
Ma7el <144173492+Ma7el@users.noreply.github.com>
2026-01-29 20:41:55 +0200

    Fix image paths to work with /zourimotors basePath

--- app/page.tsx
+++ app/page.tsx
@@ -6,104 +6,107 @@ import { Shield, Truck, Award, Star, ShoppingCart } from 'lucide-react'
 import { useCart } from '@/contexts/CartContext'
 import Image from 'next/image'
 
+const basePath = '/zourimotors'
+const getImagePath = (path: string) => `${basePath}${path}`
+
 const PRODUCTS = [
   {
     id: 1,
     name: 'مرسيدس بنز',
     brand: 'Mercedes-Benz',
     price: '99 دينار',
-    image: '/products/mercedes-benz.png',
+    image: getImagePath('/products/mercedes-benz.png'),
   },
   {
     id: 2,
     name: 'بي إم دبليو',
     brand: 'BMW',
     price: '99 دينار',
-    image: '/products/bmw.png',
+    image: getImagePath('/products/bmw.png'),
   },
   {
     id: 3,
     name: 'أودي',
     brand: 'Audi',
     price: '99 دينار',
-    image: '/products/audi.png',
+    image: getImagePath('/products/audi.png'),
   },
   {
     id: 4,
     name: 'تويوتا',
     brand: 'Toyota',
     price: '99 دينار',
-    image: '/products/toyota.png',
+    image: getImagePath('/products/toyota.png'),
   },
   {
     id: 5,
     name: 'فولكسفاغن',
     brand: 'Volkswagen',
     price: '99 دينار',
-    image: '/products/volkswagen.png',
+    image: getImagePath('/products/volkswagen.png'),
   },
   {
     id: 6,
     name: 'كيا 2',
     brand: 'Kia 2',
     price: '99 دينار',
-    image: '/products/kia2.png',
+    image: getImagePath('/products/kia2.png'),
   },
   {
     id: 7,
     name: 'مرسيدس بنز 2',
     brand: 'Mercedes-Benz 2',
     price: '99 دينار',
-    image: '/products/merceds-benz2.png',
+    image: getImagePath('/products/merceds-benz2.png'),
   },
   {
     id: 8,
     name: 'شيفروليه',
     brand: 'Chevrolet',
     price: '99 دينار',
-    image: '/products/cheverolet.png',
+    image: getImagePath('/products/cheverolet.png'),
   },
   {
     id: 9,
     name: 'ميني كوبر',
     brand: 'Mini Cooper',
     price: '99 دينار',
-    image: '/products/mini-cooper.png',
+    image: getImagePath('/products/mini-cooper.png'),
   },
   {
     id: 10,
     name: 'جينيسيس',
     brand: 'Genesis',
     price: '99 دينار',
-    image: '/products/genisis.png',
+    image: getImagePath('/products/genisis.png'),
   },
   {
     id: 11,
     name: 'دودج',
     brand: 'Dodge',
     price: '99 دينار',
-    image: '/products/dodge.png',
+    image: getImagePath('/products/dodge.png'),
   },
   {
     id: 12,
     name: 'سامسونج',
     brand: 'Samsung',
     price: '99 دينار',
-    image: '/products/samsung.png',
+    image: getImagePath('/products/samsung.png'),
   },
   {
     id: 13,
     name: 'هنداي',
     brand: 'Hundai',
     price: '99 دينار',
-    image: '/products/hundai.png',
+    image: getImagePath('/products/hundai.png'),
   },
   {
     id: 14,
     name: 'كيا',
     brand: 'Kia',
     price: '99 دينار',
-    image: '/products/kia.png',
+    image: getImagePath('/products/kia.png'),
   },
 ]
 
@@ -135,7 +138,7 @@ export default function Home() {
           <div className="mb-12 flex justify-center">
             <div className="relative w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80">
               <Image
-                src="/zourilogo.png"
+                src={getImagePath('/zourilogo.png')}
                 alt="ZOURI MOTORS Logo"
                 fill
                 className="object-contain"
@@ -218,7 +221,7 @@ export default function Home() {
                   {/* Product Image Container */}
                   <div className="relative w-full h-48 bg-neutral-900">
                     <Image
-                      src={product.image}
+                      src={getImagePath(product.image)}
                       alt={product.name}
                       fill
                       className="object-contain"
